ecc96b4b750e2a91dd09b400e4dc3eeb88ad7f89
Phosawas <phosawas@siamsaison.com>
2023-07-25 16:02:11 +0700

    change script

--- scripts/dependencies_install.sh
+++ scripts/dependencies_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export PATH=/home/ubuntu/.rvm/gems/ruby-2.7.8/bin:/home/ubuntu/.rvm/gems/ruby-2.7.8@global/bin:/home/ubuntu/.rvm/rubies/ruby-2.7.8/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.rvm/bin:/home/ubuntu/.rvm/bin
-sudo kill -9 $(cat /var/www/eb-rails/tmp/pids/server.pid)
+# sudo kill -9 $(cat /var/www/eb-rails/tmp/pids/server.pid)
 cd /var/www/eb-rails/
 #sudo apt-get install ruby2.3-dev libffi-dev -y
 gem install nokogiri — — use-system-libraries
 bundle config build.nokogiri — use-system-libraries
-gem install bundler — user-install
+gem install bundler -v 2.1.4 — user-install
 bundle install
