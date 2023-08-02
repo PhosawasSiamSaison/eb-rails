b092a1e5a21c7bf24aa984eb11bbc77f909aacf6
Phosawas <phosawas@siamsaison.com>
2023-08-02 15:40:42 +0700

    add env config

--- scripts/set_environment_variables.sh
+++ scripts/set_environment_variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function set_parameter {
-SSM_PARAM_NAME=$1
-SSM_VALUE=`aws ssm get-parameters — with-decryption — names “${SSM_PARAM_NAME}” — query ‘Parameters[*].Value’ — output text`
-[ “$(eval echo “$”$1)” == “” ] && echo “export ${SSM_PARAM_NAME}=${SSM_VALUE}” >> ~/.bashrc
+  SSM_PARAM_NAME=$1
+  SSM_VALUE=`aws ssm get-parameters --name "SECRET_KEY_BASE" --query 'Parameters[*].Value' --output text`
+  [ "$(eval echo "$"$1)" == "" ] && echo "export ${SSM_PARAM_NAME}=${SSM_VALUE}" >> ~/.bashrc
 }
 set_parameter "SECRET_KEY_BASE"
