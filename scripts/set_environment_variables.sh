3431ad44f0544dc3a394c6bd4429164e8d6ed85c
Phosawas <phosawas@siamsaison.com>
2023-08-02 15:51:54 +0700

    config env

--- scripts/set_environment_variables.sh
+++ scripts/set_environment_variables.sh
@@ -4,4 +4,5 @@ function set_parameter {
   SSM_VALUE=`aws ssm get-parameters --name "SECRET_KEY_BASE" --query 'Parameters[*].Value' --output text`
   [ "$(eval echo "$"$1)" == "" ] && echo "export ${SSM_PARAM_NAME}=${SSM_VALUE}" >> ~/.bashrc
 }
+echo "export RAILS_ENV=production" >> ~/.bashrc
 set_parameter "SECRET_KEY_BASE"
