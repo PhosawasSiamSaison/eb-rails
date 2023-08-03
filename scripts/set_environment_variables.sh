552e445a72880119715c7f38904b7a391da2f1ea
Phosawas <phosawas@siamsaison.com>
2023-08-03 10:40:52 +0700

    remove staging

--- scripts/set_environment_variables.sh
+++ scripts/set_environment_variables.sh
@@ -4,5 +4,5 @@ function set_parameter {
   SSM_VALUE=`aws ssm get-parameters --name "${SSM_PARAM_NAME}" --query 'Parameters[*].Value' --output text`
   [ "$(eval echo "$"$1)" == "" ] && echo "export ${SSM_PARAM_NAME}=${SSM_VALUE}" >> ~/.bashrc
 }
-echo "export RAILS_ENV=staging" >> ~/.bashrc
-set_parameter "SECRET_STAGING_KEY_BASE"
+echo "export RAILS_ENV=production" >> ~/.bashrc
+set_parameter "SECRET_KEY_BASE"
