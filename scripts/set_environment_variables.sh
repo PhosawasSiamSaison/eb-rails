6c83c18e9ee7556a4bb20599622b7a2fe10c5498
Phosawas <phosawas@siamsaison.com>
2023-08-03 10:14:29 +0700

    change env to staging

--- scripts/set_environment_variables.sh
+++ scripts/set_environment_variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 function set_parameter {
   SSM_PARAM_NAME=$1
-  SSM_VALUE=`aws ssm get-parameters --name "SECRET_KEY_BASE" --query 'Parameters[*].Value' --output text`
+  SSM_VALUE=`aws ssm get-parameters --name "${SSM_PARAM_NAME}" --query 'Parameters[*].Value' --output text`
   [ "$(eval echo "$"$1)" == "" ] && echo "export ${SSM_PARAM_NAME}=${SSM_VALUE}" >> ~/.bashrc
 }
-echo "export RAILS_ENV=production" >> ~/.bashrc
-set_parameter "SECRET_KEY_BASE"
+echo "export RAILS_ENV=staging" >> ~/.bashrc
+set_parameter "SECRET_STAGING_KEY_BASE"
